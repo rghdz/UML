c83493eccca62c06714a7cc594cfac9c243e8c0c
Riana ghd <ri.ghadamzadeh@gmail.com>
2025-09-19 11:03:36 +0200

    arv klassar

--- UML/Staff.cs
+++ UML/Staff.cs
@@ -8,3 +8,35 @@ public class Staff
         Console.WriteLine($"{Id} is now checked in!");
     }
 }
+
+
+public class Doctor : Staff
+{
+    public string Speciality;
+    public string Departments;
+
+    public void OrdersTreatment()
+    {
+        Console.WriteLine($"Doctor {Name} ordered {OrdersTreatment}");
+    }
+
+    public void GivesMedecin()
+    {
+        Console.WriteLine($"Doctor {Name} ordered {GivesMedecin}");
+    }
+}
+
+public class Nurse : Staff
+{
+    public string Departments;
+
+    public void HelpsDoctors()
+    {
+        Console.WriteLine($"Nurse {Name} helped out doctor {Name} with a patient!");
+    }
+
+    public void RunsTests()
+    {
+        Console.WriteLine($"Nurse {Name} ran {RunsTests} on the patient!");
+    }
+}
